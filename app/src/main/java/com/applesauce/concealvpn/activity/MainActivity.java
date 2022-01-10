254052c9b41c45b36ee5b7f8510e520c95ca47a3
Shanur Rahman <shanur.cse.nitap@gmail.com>
2022-01-11 00:33:43 +0530

    intermediates changes

---
+++ app/src/main/java/com/applesauce/concealvpn/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.applesauce.concealvpn.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.applesauce.concealvpn.utils.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
